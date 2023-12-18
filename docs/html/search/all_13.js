a10f08f9d1601e6fa19e66bb6f4eee0aed07f2ef
sourabhwarrier <sourabhw7@gmail.com>
2023-12-18 21:27:05 +0530

    Updated

---
+++ docs/html/search/all_13.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['the_20node_20type_0',['The Node Type',['../index.html#autotoc_md2',1,'']]],
+  ['to_20explore_20more_20list_20functions_20go_20to_20strong_20file_20strong_20strong_20file_20members_20strong_20strong_20functions_20strong_1',['To explore more list functions go to &lt;strong&gt;File&lt;/strong&gt; &gt; &lt;strong&gt;File members&lt;/strong&gt; &gt;  &lt;strong&gt;Functions&lt;/strong&gt;.',['../index.html#autotoc_md9',1,'']]],
+  ['to_20strong_20file_20strong_20strong_20file_20members_20strong_20strong_20functions_20strong_2',['To explore more list functions go to &lt;strong&gt;File&lt;/strong&gt; &gt; &lt;strong&gt;File members&lt;/strong&gt; &gt;  &lt;strong&gt;Functions&lt;/strong&gt;.',['../index.html#autotoc_md9',1,'']]],
+  ['type_3',['The Node Type',['../index.html#autotoc_md2',1,'']]]
+];
